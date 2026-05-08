ec89ab56b8000734f5ca64eb4892bad5ca14e2b8
Saulo Elias <sauloelias.santos010@gmail.com>
2026-05-08 07:57:15 -0300

    update3

--- app/rNative-api/app/index.js
+++ app/rNative-api/app/index.js
@@ -1,6 +1,27 @@
 import { Text, View } from "react-native";
+import { useEffect, useState} from "react";
+
 
 export default function Index() {
+  const [msg, steMsg] = useState("Loading...");
+  const [lista, setLista] = useState([]);
+
+  useEffect(() => {
+    const ip = '127.0.0.1'; // IP do servidor (localhost)
+
+    // busca rota 1
+    fetch(`http://${ip}:4000/dados`)
+      .then(res => res.json())
+      .then(json => setMsg(json.message));
+    // busca rota 2
+    fetch(`http://${ip}:4000/lista`)
+      .then(res => res.json())
+      .then(json => setLista(json));
+  }, []); // o array vazio [] garante que o useEffect seja executado apenas uma vez, quando o componente for montado
+
+
+
+
   return (
     <View
       style={{
